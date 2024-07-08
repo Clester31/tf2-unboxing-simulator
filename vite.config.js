257ab4152cbdec26d0103f8c0d669d9e92ac7d67
Clester31 <christianlester114@gmail.com>
2024-07-08 17:35:59 -0400

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tf2-crate-unboxing-sim.git/'
+  base: '/tf2-unboxing-simulator/'
 })
